ba8b3293f54aa2bfa215e7731c4d6c499fe735e2
Alan Maldonado <business.alanm@gmail.com>
2019-07-06 22:47:39 -0500

    created-videofeed-file

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
 import { Container, Row, Col } from 'react-bootstrap';
+import VideoFeed from './components/VideoFeed/VideoFeed';
 
 class App extends React.Component {
   handleUserInput = term => {
@@ -14,6 +15,15 @@ class App extends React.Component {
           <Row>
             <Col sm={true}>
               <SearchBar onUserSubmit={this.handleUserInput} />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={8} sm>
+              video feed
+              <VideoFeed />
+            </Col>
+            <Col md={4} sm>
+              video recommendations
             </Col>
           </Row>
         </Container>
