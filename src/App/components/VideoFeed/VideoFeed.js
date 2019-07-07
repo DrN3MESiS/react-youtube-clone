9558d15b634ef5541b7789c40d837fa6e49e6d62
Alan Maldonado <business.alanm@gmail.com>
2019-07-06 22:56:18 -0500

    video-feed-layout

--- src/App/components/VideoFeed/VideoFeed.js
+++ src/App/components/VideoFeed/VideoFeed.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './style.css';
 
 const VideoFeed = props => {
-  return <React.Fragment />;
+  return (
+    <React.Fragment>
+      <div className="video-feed-div">
+        <div className="video">video</div>
+        <div className="description">desc</div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default VideoFeed;
