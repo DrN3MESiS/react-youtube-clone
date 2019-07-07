711a6c1864ac6b609a0d33326bdf0be36bb96197
Alan Maldonado <business.alanm@gmail.com>
2019-07-06 21:33:44 -0500

    update-v1

--- src/App/components/SearchBar/SearchBar.js
+++ src/App/components/SearchBar/SearchBar.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 class SearchBar extends React.Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
 
   render() {
-    return <React.Fragment />;
+    return (
+      <React.Fragment>
+        <div>
+          <Button />
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
