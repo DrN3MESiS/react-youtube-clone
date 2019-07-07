7654106b2df34676cecec86cf21e7c9df53bf9f5
Alan Maldonado <business.alanm@gmail.com>
2019-07-06 22:30:29 -0500

    added-coms-btw-app-searchbar

--- src/App/components/SearchBar/SearchBar.js
+++ src/App/components/SearchBar/SearchBar.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import './style.css';
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      text: '',
+    };
   }
+
+  handleInput = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onUserSubmit(this.state.text);
+  };
 
   render() {
     return (
       <React.Fragment>
-        <div>
-          <Button />
+        <div align="CENTER" className="search-bar-div">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="search-bar"
+              onChange={this.handleInput}
+            />
+          </form>
         </div>
       </React.Fragment>
     );
